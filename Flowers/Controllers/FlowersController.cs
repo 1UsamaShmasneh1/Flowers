ff7b8a631ffb3f22a1cfac9b21131a098ca368fb
usama shmasneh <us.05.07.91@gmail.com>
2022-05-20 18:25:43 +0300

    Add project files.

---
+++ Flowers/Controllers/FlowersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flowers.Controllers
+{
+    [Route("api/[action]")]
+    [ApiController]
+    public class FlowersController : ControllerBase
+    {
+        public string Aster()
+        {
+            return "Aster";
+        }
+        public string Azalea()
+        {
+            return "Azalea";
+        }
+        public string Carnations()
+        {
+            return "Carnations";
+        }
+        public string Crocus()
+        {
+            return "Crocus";
+        }
+    }
+}
